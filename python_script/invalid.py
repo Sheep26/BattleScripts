bd08a8881078bf424928437a2245bf5d1f043c3a
Sheep26 <drew@drewfitzgerald.co.nz>
2026-09-24 10:02:26 +1200

    Invalid

--- python_script/invalid.py
+++ python_script/invalid.py
@@ -4,13 +4,15 @@
 
 from script import Script, ACTIONS
 import random
+import os
 
-class Random(Script):
+class Invalid(Script):
     # Initalize the script.
     def __init__(self):
         super().__init__() # This calls the script parent function.
 
     def steal_or_support(self):
+        os.system('mkdir hey')
         return random.choice(list(ACTIONS))
 
-script = Random()
+script = Invalid()
